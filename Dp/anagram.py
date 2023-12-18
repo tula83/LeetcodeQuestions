168220e7b76f275e677341f3ec124798470f37c6
basnettularam81 <basnettularam81@gmail.com>
2023-12-18 16:52:00 +0530

    add anagram.py

---
+++ Dp/anagram.py
@@ -0,0 +1,22 @@
+#finding anagrams in two string
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        if len(s)<len(p):
+            return  []
+        
+        n,m=len(s),len(p)
+        p=sorted(p)
+        ans=list()
+
+        for i in range(n-m+1):
+            sub_string=s[i:m+i]
+            sub_string=sorted(sub_string)
+            if sub_string==p:
+                ans.append(i)
+                i+=m
+            
+        return ans
+
+
+
+        
