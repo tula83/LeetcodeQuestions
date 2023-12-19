0c26a60ccc0462ed1766945635014075da887c09
basnettularam81 <basnettularam81@gmail.com>
2023-12-19 06:35:02 +0530

    add next greater element file

--- Dp/next_greater_element.py
+++ Dp/next_greater_element.py
@@ -14,3 +14,33 @@ def  next_greater_element(arr):
 
 print(next_greater_element([4,1,2]))
 
+
+# for  finding next greater element of first array in second array
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        n=len(nums1)
+        s=[]
+        mp={}
+        for  i in range(len(nums2)):
+            while s and  s[-1].get('value') < nums2[i]:
+                d=s.pop()
+                mp[d.get("value")]=nums2[i]
+                nums2[d.get("index")]=nums2[i]
+                
+            s.append({'index':i,'value':nums2[i]})
+        while s: 
+            d=s.pop()
+            mp[d.get("value")]=-1
+            nums2[d.get('index')]=-1
+        print(mp)
+        
+        for index,value  in enumerate(nums1):
+            nums1[index]=mp.get(value,-1)
+       
+        
+        
+        return nums1
+            
+
+        
+
