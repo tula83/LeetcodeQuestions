7d8434976f965462587fb7c20237f88ba8eb62cb
basnettularam81 <basnettularam81@gmail.com>
2023-12-24 17:35:00 +0530

    add folder for practising questions

--- Practice_Questions/product_less_than_k.py
+++ Practice_Questions/product_less_than_k.py
@@ -19,5 +19,31 @@ def product_less_than_k(nums,k):
             res+=i-start+1
     return res
 
-print(product_less_than_k([10,5,2,6],100))
+#print(product_less_than_k([10,5,2,6],100))
+
+
+# related question  Subarray Sum Equals K
+#using  dictionary sum
+
+def Subarray_sum_equals_k(nums,k):
+
+    #n=len(nums)
+    dict_sum={0:1}
+    sum=0
+    count=0
+
+    for item in nums:
+
+        sum+=item
+
+        count+=dict_sum.get(sum-k,0)
+
+        dict_sum[sum]=dict_sum.get(sum,0)+1
+    
+    return count
+
+print(Subarray_sum_equals_k([1,1,1],2))
+
+
+
 
