ea10d86474654a9f306f614460d0ebd8aff49248
basnettularam81 <basnettularam81@gmail.com>
2023-12-27 11:43:04 +0530

    add contiguous array

--- Practice_Questions/contiguous_array.py
+++ Practice_Questions/contiguous_array.py
@@ -22,4 +22,81 @@ def findMaxlength(arr):
 
 
 
-print(findMaxlength([1,0,1]))
+#print(findMaxlength([1,0,1]))
+
+
+
+def subarraysDivByK(self, nums, k):
+
+        n=len(nums)
+        count=0
+        
+        for i in range(n):
+            for  j in range(i,n):
+                if i==j:
+                    if nums[i]%k==0:
+                        count+=1
+                else:
+                    result=sum(nums[i:j+1])
+                   
+                    if result%k==0:
+                        
+                        count+=1
+                
+                
+               
+                
+                
+        
+        return count
+
+
+
+
+def subarrayDivByK(arr,k):
+
+    sum_remainder_freq={0:1}
+    running_sum=0
+    count=0
+
+
+    for num in arr:
+        running_sum=(running_sum+num)%k
+        if running_sum < 0:
+            running_sum+=k
+
+
+        if running_sum in sum_remainder_freq:
+            count+=sum_remainder_freq.get(running_sum)
+
+        
+        sum_remainder_freq[running_sum]=sum_remainder_freq.get(running_sum,0)+1
+
+    return count
+
+
+
+#print(subarrayDivByK([4,5,0,-2,-3,1],5))
+
+#You are given a 0-indexed integer array nums. A pair of indices (i, j) is a bad pair if i < j and j - i != nums[j] - nums[i].
+#Return the total number of bad pairs in nums.
+import collections
+def count_bad_pairs(nums):
+    n=len(nums)
+    result=n*(n-1)//2
+
+    cnt=collections.Counter()
+    
+    
+    for i ,x in enumerate(nums):
+        print(f'x - i : {x-i}')
+        result-=cnt[x-i]
+        cnt[x-i]+=1
+        print(f"i: {i}, x: {x}, result: {result}, cnt: {cnt}")
+    print(cnt)
+    return result
+
+
+print(count_bad_pairs([4,1,3,3]))
+
+
